40ee1a3a17b7e06b00288df42647d2018b038e37
Benji <benjaminpeto23@gmail.com>
2021-11-25 15:10:49 +0100

    created react app, cleaned up unwanted code, imported webfonts and assets, structured folders

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './fonts/Gilroy-Medium.ttf';
+import './fonts/Gilroy-ExtraBold.ttf';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
