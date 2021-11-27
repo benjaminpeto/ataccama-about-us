00c03d92ac2b24180e59282bc8e657d56d725f71
Benji <benjaminpeto23@gmail.com>
2021-11-27 09:26:42 +0100

    added gradient on cardSlider component, moved arrow controls from wheretofind component to cardslider, and change those stylings

--- src/components/CardSlider/CardSlider.components.jsx
+++ src/components/CardSlider/CardSlider.components.jsx
@@ -2,15 +2,24 @@ import React from "react";
 
 import Card from '../Cards/Card.components';
 
+import { ReactComponent as ArrowLeft } from '../../assets/Arrow left.svg';
+import { ReactComponent as ArrowRight } from '../../assets/Arrow right.svg';
+
 import './CardSlider.styles.scss';
 
 function  CardSlider() {
   return (
-    <div className='radient-cover'>
-      <div className='card-slider'>
-        <Card />
+    <>
+      <div className='arrows'>
+          <ArrowLeft className='arrow-left' />
+          <ArrowRight className='arrow-right' />
+        </div>
+      <div className='radient-cover'>
+        <div className='card-slider'>
+          <Card />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
